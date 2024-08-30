581204972883b4010c1e19164b6aeaae6ec356bc
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-08-30 19:30:37 +0330

    add animation to friends list in right aside

--- main.js
+++ main.js
@@ -71,3 +71,35 @@ const navLinkClickHandler = (event) => {
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', navLinkClickHandler);
 });
+
+//Add Animation to Friends Avatar List in Right Aside
+const friendsAvatar = document.querySelectorAll('.friends div a');
+
+friendsAvatar.forEach(friend => {
+    friend.addEventListener('mouseover', (event) => {
+        if(event.target.tagName === 'A'){
+            console.log(event.target);
+            for(let i = 0; i < friendsAvatar.length; i++){
+                if(friendsAvatar[i].isEqualNode(event.target)){
+                    friendsAvatar[i].style.marginInlineEnd = "20px";
+                }
+            }
+        }
+        
+    })
+})
+
+friendsAvatar.forEach(friend => {
+    friend.addEventListener('mouseleave', (event) => {
+        if(event.target.tagName === 'A'){
+            console.log(event.target);
+            for(let i = 0; i < friendsAvatar.length; i++){
+                if(friendsAvatar[i].isEqualNode(event.target)){
+                    friendsAvatar[i].style.marginInlineEnd = "unset";
+                }
+            }
+        }
+        
+    })
+})
+//#End //Add Animation to Friends Avatar List in Right Aside
